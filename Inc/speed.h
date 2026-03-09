fa3a60b46ce2a02c25245f5f61a0a21ea5cfef12
raven-sm79 <raven.sm79@gmail.com>
2026-03-10 00:36:21 +0500

    Update speed.h

--- Inc/speed.h
+++ Inc/speed.h
@@ -7,7 +7,8 @@ void     SPEED_Init(uint16_t pulse_rem);
 void 	 SPEED_OnPulseFilteredISR(void);
 
 uint32_t SPEED_GetKmPending(void);
-void SPEED_ConsumeKm(uint32_t n);
+//void SPEED_ConsumeKm(uint32_t n);
 
 uint16_t SPEED_GetPulseRem(void);
 void     SPEED_SetPulseRem(uint16_t rem);
+
